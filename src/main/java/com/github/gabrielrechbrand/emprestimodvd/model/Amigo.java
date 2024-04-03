7775e86cab3c4b547eb0500fcdbe631c19756d10
Gabriel <rcgabrielbrand@gmail.com>
2024-04-03 20:33:04 -0300

    dev/database - Implementing "Amigo" as an entity.

---
+++ src/main/java/com/github/gabrielrechbrand/emprestimodvd/model/Amigo.java
@@ -0,0 +1,31 @@
+package com.github.gabrielrechbrand.emprestimodvd.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Amigo extends Pessoa {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column
+    private String telefone;
+
+    @Column
+    private String email;
+
+    @Column
+    private String endereco;
+
+    @OneToOne
+    @JoinColumn(name = "pessoa_id", referencedColumnName = "id")
+    private Pessoa pessoa;
+
+}
