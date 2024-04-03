4303d29962a53cbeebd8e32cd40427b4430e8d68
vini2003 <vini.sch@outlook.com>
2024-04-03 20:26:01 -0300

    dev/database - Mock traditional initialization in EmprestimoDvdApplication as we cannot statically initialize it like usual. Defer execution to a different thread and delay it by 5 seconds.

--- src/main/java/com/github/gabrielrechbrand/emprestimodvd/EmprestimoDvdApplication.java
+++ src/main/java/com/github/gabrielrechbrand/emprestimodvd/EmprestimoDvdApplication.java
@@ -5,8 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmprestimoDvdApplication {
+    private static boolean executado = false;
+
     public static void main(String[] args) {
         SpringApplication.run(EmprestimoDvdApplication.class, args);
+    }
+
+    public EmprestimoDvdApplication() {
+        execute();
+    }
+
+    public static void execute() {
+        if (executado) return;
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(5_000);
+
+                cadastraAmigos();
+                cadastraDVDs();
+                registraEmprestimos();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).run();
     }
 
     public static void cadastraAmigos() {
