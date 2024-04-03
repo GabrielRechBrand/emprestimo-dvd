9c6b8605214ce9fd937037556e03b02c238e337c
Gabriel <rcgabrielbrand@gmail.com>
2024-04-03 19:35:35 -0300

    master - Initial commit

---
+++ src/main/java/com/github/gabrielrechbrand/emprestimodvd/EmprestimoDvdApplication.java
@@ -0,0 +1,13 @@
+package com.github.gabrielrechbrand.emprestimodvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoDvdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmprestimoDvdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/gabrielrechbrand/emprestimodvd/EmprestimoDvdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielrechbrand.emprestimodvd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoDvdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
